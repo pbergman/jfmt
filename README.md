97b167e8808ac63c23ed0a0122d351318639ab0d
philip <philip@zicht.nl>
2018-05-24 10:37:14 +0200

    error handling and support multiple template args

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ### JFMT
 
-jfmt (json formatting) is a small program without any dependencies that can format or print json input. It uses the standard [go text/template](https://golang.org/pkg/text/template)
-for formatting the output and if no template argument was provided then it will pretty print the json input to the stdout.
+jfmt (json formatting) is a small program without any dependencies that can format or print json input.
+
+It uses the standard [go text/template](https://golang.org/pkg/text/template) for formatting the output and if no template argument was provided then it will pretty print the json input to the stdout.
 
 #### install
 
@@ -13,4 +14,18 @@ make build && mv jfmt /usr/local/bin
 echo "[{"name": "foo"},{"name": "bar:}]" | jfmt "{{ range . }}>{{ .name }}\n{{end}}"
 >foo
 >bar
+```
+
+or using the template blocks with multiple arguments:
+
+```
+echo "[{"name": "foo"},{"name": "bar:}]" | jfmt '{{define "dumper"}}{{ . | dump }}{{end}}' '{{template "dumper" .}}'
+[
+    {
+        "name": "foo"
+    },
+    {
+        "name": "bar"
+    }
+]
 ```
--- main.go
+++ main.go
@@ -13,6 +13,11 @@ var (
 	version, ref string
 )
 
+func exit(msg string, code int) {
+	fmt.Fprintln(os.Stderr, msg)
+	os.Exit(code)
+}
+
 func getData() interface{} {
 
 	var data interface{}
@@ -20,15 +25,17 @@ func getData() interface{} {
 	fi, err := os.Stdin.Stat()
 
 	if err != nil {
-		panic(err)
+		exit(err.Error(), 1)
 	}
 
 	if fi.Mode()&os.ModeCharDevice == 0 {
 		dec := json.NewDecoder(os.Stdin)
 
 		if err := dec.Decode(&data); err != nil {
-			panic(err)
+			exit(err.Error(), 2)
 		}
+	} else {
+		exit("missing json input", 3)
 	}
 
 	return data
@@ -37,7 +44,7 @@ func getData() interface{} {
 func usage() {
 	fmt.Printf(`jfmt %[2]s(%[3]s)
 
-usage: %[1]s [-h|help] [template] <INPUT
+usage: %[1]s [-h|help] [template...] <INPUT
 
 This is a small program that format json input. It uses the standard go text/template for formating the
 output and if no template argument was provided then it will prity print the json input to the stdout.
@@ -48,59 +55,82 @@ echo '[{"name": "foo"},{"name": "bar"}]' | %[1]s '{{ range . }}{{ .names }}\n{{e
 `, os.Args[0], version, ref)
 }
 
+func dump(v interface{}) string {
+	out, err := json.MarshalIndent(v, "", "    ")
+
+	if err != nil {
+		exit(err.Error(), 8)
+	}
+
+	return string(out)
+}
+
+func getTmplFuncs() map[string]interface{} {
+	return map[string]interface{}{
+		"dump": dump,
+	}
+}
+
+func fmtResult(tmpl *template.Template, source string, data interface{}) {
+
+	var err error
+
+	if tmpl, err = tmpl.Parse(source); err != nil {
+		exit(err.Error(), 4)
+	}
+
+	buf := new(bytes.Buffer)
+	err = tmpl.Execute(buf, data)
+
+	if err != nil {
+		exit(err.Error(), 5)
+	}
+
+	pattern := regexp.MustCompile(`\\[tnvf]`)
+	output := buf.String()
+
+	if pattern.MatchString(buf.String()) {
+		output = pattern.ReplaceAllStringFunc(output, func(match string) string {
+			switch match[1] {
+			case 't':
+				return "\t"
+			case 'n':
+				return "\n"
+			case 'v':
+				return "\v"
+			case 'f':
+				return "\f"
+			}
+			return ""
+		})
+	}
+
+	fmt.Fprintln(os.Stdout, output)
+}
+
+func hasHelp(size int, args []string) bool {
+	for i := 0; i < size; i++ {
+		if args[i] == "-h" || args[i] == "help" {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
-
 	data := getData()
+	args := os.Args[1:]
 
-	if len(os.Args) > 1 {
-
-		if os.Args[1] == "-h" || os.Args[1] == "help" {
+	if size := len(args); size > 1 {
+		if hasHelp(size, args) {
 			usage()
-			return
+		} else {
+			tmpl := template.New("out").Option("missingkey=zero").Funcs(getTmplFuncs())
+			for i := 0; i < size; i++ {
+				fmtResult(tmpl, args[i], data)
+			}
 		}
-
-		tmpl, err := template.New("out").Parse(os.Args[1])
-
-		if err != nil {
-			panic(err)
-		}
-
-		buf := new(bytes.Buffer)
-
-		err = tmpl.Execute(buf, data)
-
-		if err != nil {
-			panic(err)
-		}
-
-		pattern := regexp.MustCompile(`\\[tnvf]`)
-		output := buf.String()
-
-		if pattern.MatchString(buf.String()) {
-			output = pattern.ReplaceAllStringFunc(output, func(match string) string {
-				switch match[1] {
-				case 't':
-					return "\t"
-				case 'n':
-					return "\n"
-				case 'v':
-					return "\v"
-				case 'f':
-					return "\f"
-				}
-				return ""
-			})
-		}
-
-		fmt.Print(output)
-
 	} else {
-		out, err := json.MarshalIndent(data, "", "    ")
-
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println(string(out))
+		fmt.Fprint(os.Stdout, dump(data))
 	}
 }
